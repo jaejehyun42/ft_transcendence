eeb7e2a0cbca5d290dbaf0a644c66dc9072f11b1
Jaebin Lee <jaebin.lee00@gmail.com>
2025-03-27 15:23:29 +0900

    feat: 프로필 저장 버튼 클래스 속성 추가

--- srcs/merge/frontend/pages/profile.ts
+++ srcs/merge/frontend/pages/profile.ts
@@ -71,7 +71,7 @@ export const profilePage = `
 
             <!-- 저장 버튼 -->
             <button data-i18n="savechanges" id="save-btn" 
-                class="w-full px-4 py-2 bg-green-500 text-white rounded-md text-lg font-semibold hover:bg-green-600">
+                class="w-full px-4 py-2 bg-green-500 text-white rounded-md text-lg font-semibold hover:bg-green-600 disabled:opacity-50 disabled:cursor-not-allowed">
             </button>
 
             <!-- 상태 메시지 표시 -->
